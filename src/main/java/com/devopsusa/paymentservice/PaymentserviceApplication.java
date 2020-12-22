f951a7cdfd8a54b11babedad0878cb8ca5644749
nirajthanki6 <nirajthanki6@gmail.com>
2020-12-21 20:34:59 -0600

    Add CI/CD CircleCI Pipeline Config

--- src/main/java/com/devopsusa/paymentservice/PaymentserviceApplication.java
+++ src/main/java/com/devopsusa/paymentservice/PaymentserviceApplication.java
@@ -2,8 +2,10 @@ package com.devopsusa.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -20,6 +22,12 @@ public class PaymentserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentserviceApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 	@Bean
